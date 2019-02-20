b37962cfe10a497123dc568d65d1b644204e6b4c
park <park@DESKTOP-27SP5R2>
2019-02-20 15:39:27 +0900

    dff

--- src/main/java/com/kh/eg/auction/controller/AuctionController.java
+++ src/main/java/com/kh/eg/auction/controller/AuctionController.java
@@ -2,6 +2,8 @@ package com.kh.eg.auction.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.eg.attachment.model.vo.Attachment;
 import com.kh.eg.auction.model.service.AuctionService;
@@ -19,10 +24,31 @@ import com.kh.eg.auction.model.vo.Pagination;
 import com.kh.eg.bidding.model.vo.Bid;
 import com.kh.eg.item.model.vo.Item;
 
+import net.sf.json.JSONArray;
+
 @Controller
 public class AuctionController {
 	@Autowired 
 	private AuctionService as;
+	
+	@RequestMapping("selectAlign.au")
+	public @ResponseBody String selectAlign(Model model,@RequestParam String alignName, @RequestBody String paramData) {
+		
+	    List<Map<String,Object>> resultMap = new ArrayList<Map<String,Object>>();
+	    resultMap = JSONArray.fromObject(paramData);
+	         
+	    for (Map<String, Object> map : resultMap) {
+	        System.out.println(map.get("user_nm") + " : " + map.get("user_password"));
+	        //관리자 : 1111
+	        //직원 : 2222
+	    }
+
+
+		
+		return null;
+	}
+	
+	
 
 	@RequestMapping("auctionCategory.au")
 	public String selectAuction(HttpServletRequest request,Model model,Item it ) {
