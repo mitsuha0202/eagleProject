27401101a66401dcf1afc67e6aee9ff92fa57ac4
park <park@DESKTOP-27SP5R2>
2019-02-21 15:10:21 +0900

    ggg

--- src/main/java/com/kh/eg/auction/model/dao/AuctionDaoImpl.java
+++ src/main/java/com/kh/eg/auction/model/dao/AuctionDaoImpl.java
@@ -39,6 +39,25 @@ public class AuctionDaoImpl implements AuctionDao {
 		if(alignName.equals("1")) {
 			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC");
 		}
+		else if(alignName.equals("2")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC2");
+		}
+		else if(alignName.equals("3")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC3");
+			
+		}
+		else if(alignName.equals("4")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC4");
+		}
+		else if(alignName.equals("5")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC5");
+		}
+		else if(alignName.equals("6")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC6");
+		}
+		else if(alignName.equals("7")) {
+			bidlist=(ArrayList)sqlSession.selectList("Item.selectBidC7");
+		}
 		return bidlist;
 	}
 
@@ -49,6 +68,27 @@ public class AuctionDaoImpl implements AuctionDao {
 		if(alignName.equals("1")) {
 			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC");
 		}
+		else if(alignName.equals("2")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC2");
+			
+		}
+		else if(alignName.equals("3")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC3");
+			
+		}
+		else if(alignName.equals("4")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC4");
+		}
+		else if(alignName.equals("5")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC5");
+		}
+		else if(alignName.equals("6")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC6");
+		}
+		else if(alignName.equals("7")) {
+			itemlist=(ArrayList)sqlSession.selectList("Item.selectItemC7");
+		}
+		
 		return itemlist;
 	}
 
@@ -57,6 +97,25 @@ public class AuctionDaoImpl implements AuctionDao {
 		ArrayList<Attachment> attachmentlist=null;
 		if(alignName.equals("1")) {
 			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC");
+		}
+		else if(alignName.equals("2")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC2");
+		}
+		else if(alignName.equals("3")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC3");
+			
+		}
+		else if(alignName.equals("4")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC4");
+		}
+		else if(alignName.equals("5")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC5");
+		}
+		else if(alignName.equals("6")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC6");
+		}
+		else if(alignName.equals("7")) {
+			attachmentlist=(ArrayList)sqlSession.selectList("Item.selectAttachmentC7");
 		}
 		return attachmentlist;
 	}
